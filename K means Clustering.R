6d6e6c4b798d8064211e767a81b53dfa19460734
turkesh <48882105+turkeshp@users.noreply.github.com>
2020-06-28 19:55:37 +0530

    Add files via upload

---
+++ K means Clustering.R
@@ -0,0 +1,20 @@
+install.packages('factoextra')
+library(factoextra)
+#Universities<-read.csv("/Volumes/Data/Course Content/DS content/Clustering/University Data Hierarchical clustering/Universities.csv")
+
+# Elbow method
+
+fviz_nbclust(Universities[,-1], kmeans, method = "wss") +
+  labs(subtitle = "Elbow method")
+
+###Cluster algorithm building
+km <- kmeans(Universities[,-1],8) 
+km$centers
+km$cluster
+clust<-data.frame("University"=Universities[,1],"cluster"=km$cluster)
+
+##Animation
+install.packages("animation")
+library(animation)
+km <- kmeans.ani(Universities[,-c(1)], 4)
+cl<-data.frame("Uni" =Universities[,1])
