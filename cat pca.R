9790b03776f4d9775bcd3d24b2fabc08b6249500
turkesh <48882105+turkeshp@users.noreply.github.com>
2020-06-30 19:22:55 +0530

    Add files via upload

---
+++ cat pca.R
@@ -0,0 +1,23 @@
+install.packages("jpeg","imager","pixmap")
+
+library(jpeg)
+library(imager)
+library(pixmap)
+
+cat <- readJPEG('/Volumes/Data/Course Content/DS content/PCA/Case study/cat.jpg')
+
+ncol(cat)
+nrow(cat)
+
+r <- cat[,,1]
+g <- cat[,,2]
+b <- cat[,,3]
+
+cat.r.pca <- prcomp(r, center = FALSE)
+cat.g.pca <- prcomp(g, center = FALSE)
+cat.b.pca <- prcomp(b, center = FALSE)
+
+rgb.pca <- list(cat.r.pca, cat.g.pca, cat.b.pca)
+pca.img <- sapply(rgb.pca, function(j) {  compressed.img <- j$x[,1:50] %*% t(j$rotation[,1:50])}, simplify = 'array')
+a<-pixmap::pixmapRGB(pca.img)
+plot(a)
